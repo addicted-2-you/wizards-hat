e88d6c8934b9090297f4879bbd2a088185eaf9bc
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2021-02-14 15:22:44 +0300

    feat: open github in the new page

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 function AppFooter() {
   return (
     <footer className="app-footer">
-      <a className="app-footer__github-link" href="https://github.com/Slnchn">
+      <a
+        className="app-footer__github-link"
+        href="https://github.com/Slnchn"
+        rel="noreferrer"
+        target="_blank"
+      >
         😻
       </a>
     </footer>
