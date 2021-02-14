ae8ae3bbc17c23d2479e855e993452fd32d2158e
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2021-02-14 14:54:23 +0300

    feat: add footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import AppHeader from 'components/AppHeader';
 import MainMenu from 'components/MainMenu';
+import AppFooter from 'components/AppFooter';
 
 function App() {
   return (
     <>
       <AppHeader />
       <MainMenu />
+      <AppFooter />
     </>
   );
 }
---
+++ src/components/AppFooter.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function AppFooter() {
+  return (
+    <footer className="app-footer">
+      <a className="app-footer__github-link" href="https://github.com/Slnchn">
+        😻
+      </a>
+    </footer>
+  );
+}
+
+export default AppFooter;
